aa778fb55132de02c0907cad2c95a200c060d9d6
TiagoLGama <tiagolobato98gama@gmail.com>
2025-06-29 23:48:40 -0300

    mover responsabilidade

--- InterfaceJogador.py
+++ InterfaceJogador.py
@@ -2,23 +2,8 @@ import tkinter as tk
 from tkinter import Menu, messagebox, simpledialog, Canvas
 from dog.dog_actor import DogActor
 from dog.dog_interface import DogPlayerInterface
-from enum import Enum, auto
-from Tabuleiro import Tabuleiro
+from Tabuleiro import Tabuleiro, JogadaFase, MatchStateEnum
 from Peca import Peca
-
-
-class MatchStateEnum(Enum):
-    NONE = auto()
-    WAITING_REMOTE = auto()
-    WAITING_INPUT = auto()
-    FINISHED_BY_VICTORY_CONDITION = auto()
-    FINISHED_BY_WITHDRAWAL = auto()
-
-class JogadaFase(Enum):
-    SELECIONAR_TOTEM = auto()
-    MOVER_TOTEM = auto()
-    ESCOLHER_SIMBOLO = auto()
-    COLOCAR_PECA = auto()
 
 
 
@@ -28,7 +13,6 @@ class InterfaceJogador(DogPlayerInterface):
         self.dog_server_interface = DogActor()
         self.menubar = Menu(self.tk)
         self.arquivo = Menu(self.menubar, tearoff=0)
-        self.match_state: MatchStateEnum = None
 
         self.canvas_width = 500
         self.canvas_height = 500
@@ -47,13 +31,8 @@ class InterfaceJogador(DogPlayerInterface):
 
         self.tabuleiro = Tabuleiro(self.grid_size)
 
-        self.fase_jogada = JogadaFase.SELECIONAR_TOTEM
-        self.totem_selecionado = None 
-        self.simbolo_escolhido = None
-
         self.composicao_jogada = {}
 
-        self.current_symbol = None
         self.current_color = None
 
         self.update_interface()
@@ -100,127 +79,27 @@ class InterfaceJogador(DogPlayerInterface):
         messagebox.showinfo(message=mensagem)
 
     def selecionar_simbolo(self, value: str):
-        if self.fase_jogada != JogadaFase.ESCOLHER_SIMBOLO:
+        if self.tabuleiro.fase_jogada != JogadaFase.ESCOLHER_SIMBOLO:
             messagebox.showerror("Jogada inválida", "Não está na vez de selecionar uma peça")
             return
 
-        self.simbolo_escolhido = value
-        self.fase_jogada = JogadaFase.COLOCAR_PECA
+        self.tabuleiro.simbolo_escolhido = value
+        self.tabuleiro.fase_jogada = JogadaFase.COLOCAR_PECA
         messagebox.showinfo("Simbolo selecionado", f"Simbolo {value} selecionado.")
-
 
 
     def on_click_event(self, event):
         x = int((event.x - self.board_margin) // self.cell_size)
         y = int((event.y - self.board_margin) // self.cell_size)
 
-        if self.match_state != MatchStateEnum.WAITING_INPUT:
-            messagebox.showerror("Jogada inválida", "Não é o seu turno!")
-            return
-
-        match self.fase_jogada:
-            case JogadaFase.SELECIONAR_TOTEM:
-                peca = self.tabuleiro.get_peca(x, y)
-                if peca and peca.type == "totem":
-                    self.totem_selecionado = peca
-                    self.fase_jogada = JogadaFase.MOVER_TOTEM
-                    messagebox.showinfo("Totem selecionado", f"Totem em ({x}, {y}) selecionado.")
-                else:
-                    messagebox.showwarning("Seleção inválida", "Selecione um Totem válido (preto).")
-
-            case JogadaFase.MOVER_TOTEM:
-                if self.tabuleiro.get_peca(x, y):
-                    messagebox.showwarning("Movimento inválido", "Já existe uma peça nessa posição.")
-                    return
+        self.composicao_jogada = self.tabuleiro.realizar_jogada(x, y, self.current_color)
+        self.update_interface()
+        if self.composicao_jogada != None:
+            self.dog_server_interface.send_move(self.composicao_jogada)
                 
-                # Verifica movimento ortogonal do totem
-                from_x = self.totem_selecionado.x
-                from_y = self.totem_selecionado.y
-
-                movimento_diagonal = from_x != x and from_y != y
-
-                if movimento_diagonal:
-                    ortogonal_disponivel = False
-
-                    # Verifica todas as casas na mesma linha (x fixo) e coluna (y fixo)
-                    for i in range(5):
-                        if i != from_y and not self.tabuleiro.get_peca(from_x, i):
-                            ortogonal_disponivel = True
-                            break
-                        if i != from_x and not self.tabuleiro.get_peca(i, from_y):
-                            ortogonal_disponivel = True
-                            break
-
-                    if ortogonal_disponivel:
-                        messagebox.showwarning("Movimento inválido", "Movimento diagonal não permitido enquanto houver casas ortogonais livres.")
-                        return
-                            
-                self.totem_selecionado.x = x
-                self.totem_selecionado.y = y
-                self.composicao_jogada = {
-                    "totem": {
-                    "x": x,
-                    "y": y,
-                    "symbol": self.totem_selecionado.symbol
-                    }
-                }
-                self.totem_selecionado = None
-
-                self.update_interface()
-                self.fase_jogada = JogadaFase.ESCOLHER_SIMBOLO
-                messagebox.showinfo("Totem movido", "Agora selecione um símbolo (X ou O).")
-
-            case JogadaFase.ESCOLHER_SIMBOLO:
-                if self.simbolo_escolhido == None:
-                    messagebox.showwarning("Movimento inválido", "Você deve selecionar um simbolo")
-                    return
-                
-            case JogadaFase.COLOCAR_PECA:
-                if self.simbolo_escolhido is None:
-                    messagebox.showwarning("Símbolo não escolhido", "Você precisa escolher X ou O antes.")
-                    return
-                if self.tabuleiro.get_peca(x, y):
-                    messagebox.showwarning("Posição ocupada", "Essa posição já está ocupada.")
-                    return
-
-                totem_x = self.composicao_jogada["totem"]["x"]
-                totem_y = self.composicao_jogada["totem"]["y"]
-
-                adjacentes_vazias = self.get_posicoes_adjacentes_vazias(totem_x, totem_y)
-
-                if adjacentes_vazias:
-                    if (x, y) not in adjacentes_vazias:
-                        messagebox.showwarning(
-                            "Movimento inválido",
-                            "Você só pode colocar a peça em uma célula adjacente vazia ao Totem movido."
-                        )
-                        return
-
-                nova_peca = self.tabuleiro.realizar_jogada(x, y, self.current_color, self.simbolo_escolhido)
-
-                condicao_vitoria_atingida = self.verificar_vitoria(x, y, self.current_color, self.simbolo_escolhido)
-
-                match_status = "next"
-                if condicao_vitoria_atingida:
-                    match_status = "finished"
-                    messagebox.showwarning("Partida finalizada", "você venceu!")
-                    self.match_state = MatchStateEnum.FINISHED_BY_VICTORY_CONDITION
-                else:
-                    self.match_state = MatchStateEnum.WAITING_REMOTE
-                    self.fase_jogada = JogadaFase.SELECIONAR_TOTEM
-                    self.simbolo_escolhido = None
-
-                self.composicao_jogada["peca"] = nova_peca
-                self.composicao_jogada["match_status"] = match_status
-            
-                self.update_interface()
-                self.dog_server_interface.send_move(self.composicao_jogada)
-                
-
 
     def start_match(self):
         self.current_color = "blue"
-        self.current_symbol = "X"
         self.atualizar_cor_botoes(self.current_color)
 
         match_status = self.tabuleiro.get_status_partida()
@@ -237,12 +116,11 @@ class InterfaceJogador(DogPlayerInterface):
                     local_player_id = start_status.get_local_id()
                     self.tabuleiro.start_match(players, local_player_id)
                     self.tabuleiro.jogador_local.alternar_turno()
-                    self.match_state = MatchStateEnum.WAITING_INPUT
+                    self.tabuleiro.match_state = MatchStateEnum.WAITING_INPUT
                     messagebox.showinfo(message=start_status.get_message())
 
     def receive_start(self, start_status):
         self.current_color = "red"
-        self.current_symbol = "O"
         self.atualizar_cor_botoes(self.current_color)
 
         self.start_game()
@@ -250,7 +128,7 @@ class InterfaceJogador(DogPlayerInterface):
         local_player_id = start_status.get_local_id()
         self.tabuleiro.start_match(players, local_player_id)
         self.game_state = self.tabuleiro.get_status_partida()
-        self.match_state = MatchStateEnum.WAITING_REMOTE
+        self.tabuleiro.match_state = MatchStateEnum.WAITING_REMOTE
         messagebox.showinfo(message="Recebe início partida")
 
     def start_game(self):
@@ -261,17 +139,17 @@ class InterfaceJogador(DogPlayerInterface):
 
     def receive_withdrawal_notification(self):
         self.game_state = self.tabuleiro.get_status_partida()
-        self.match_state = MatchStateEnum.FINISHED_BY_WITHDRAWAL
+        self.tabuleiro.match_state = MatchStateEnum.FINISHED_BY_WITHDRAWAL
         messagebox.showinfo("Vitória por desistência", "Adversário abandonou a partida!")
 
     def receive_move(self, a_move: dict):
         self.tabuleiro.receive_move(a_move)
         if a_move["match_status"] == "finished":
-            self.match_state = MatchStateEnum.FINISHED_BY_VICTORY_CONDITION
+            self.tabuleiro.match_state = MatchStateEnum.FINISHED_BY_VICTORY_CONDITION
             messagebox.showinfo("Partida Finalizada", "Você foi derrotado!")
         else :
             self.game_state = self.tabuleiro.get_status_partida()
-            self.match_state = MatchStateEnum.WAITING_INPUT
+            self.tabuleiro.match_state = MatchStateEnum.WAITING_INPUT
         
         self.update_interface()
 
@@ -327,49 +205,3 @@ class InterfaceJogador(DogPlayerInterface):
     def atualizar_cor_botoes(self, cor):
         self.botao_x.config(bg=cor)
         self.botao_o.config(bg=cor)
-
-    def get_posicoes_adjacentes_vazias(self, x: int, y: int):
-        adjacentes = []
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-                nx, ny = x + dx, y + dy
-                if dx == 0 and dy == 0:
-                    continue
-                if 0 <= nx < self.grid_size and 0 <= ny < self.grid_size:
-                    if not self.tabuleiro.get_peca(nx, ny):
-                        adjacentes.append((nx, ny))
-        return adjacentes
-
-    def verificar_vitoria(self, x, y, cor, simbolo):
-        direcoes = [
-            (1, 0),  
-            (0, 1),  
-            (1, 1),  
-            (1, -1), 
-        ]
-
-        for dx, dy in direcoes:
-            cont_cor = 1
-            cont_simbolo = 1
-
-            for sentido in [-1, 1]:
-                nx, ny = x, y
-                while True:
-                    nx += dx * sentido
-                    ny += dy * sentido
-                    if 0 <= nx < 5 and 0 <= ny < 5:
-                        peca = self.tabuleiro.get_peca(nx, ny)
-                        if not peca or peca.type == "totem":
-                            break
-
-                        if peca.color == cor:
-                            cont_cor += 1
-                        if peca.symbol == simbolo:
-                            cont_simbolo += 1
-                    else:
-                        break
-
-            if cont_cor >= 4 or cont_simbolo >= 4:
-                return True
-
-        return False
--- Tabuleiro.py
+++ Tabuleiro.py
@@ -1,4 +1,5 @@
-from typing import Optional 
+from typing import Optional
+from tkinter import messagebox
 from enum import Enum, auto
 from Peca import Peca
 import Jogador as jogador
@@ -11,6 +12,19 @@ import Jogador as jogador
 # 5 - NOT your turn, match in progress - waiting move
 # 6 - match abandoned by opponent
 
+class MatchStateEnum(Enum):
+    NONE = auto()
+    WAITING_REMOTE = auto()
+    WAITING_INPUT = auto()
+    FINISHED_BY_VICTORY_CONDITION = auto()
+    FINISHED_BY_WITHDRAWAL = auto()
+
+class JogadaFase(Enum):
+    SELECIONAR_TOTEM = auto()
+    MOVER_TOTEM = auto()
+    ESCOLHER_SIMBOLO = auto()
+    COLOCAR_PECA = auto()
+
 class Tabuleiro:
     def __init__(self, grid_size = 5):
         self.jogador_local = jogador.Jogador()
@@ -18,16 +32,122 @@ class Tabuleiro:
         self.jogador_local.inicializar(1, "Red player", "Red player")
         self.jogador_remoto.inicializar(2, "Blue player", "Blue player")
         self.grid_size = grid_size
+        self.match_state = None
+        self.fase_jogada = JogadaFase.SELECIONAR_TOTEM
+        
+        self.totem_selecionado = None
+        self.simbolo_escolhido = None
 
         self.status_partida = 1
-
+    
     
         self.pieces = [
             Peca(0, 0, "black", "X", "totem"),
             Peca(1, 1, "black", "O", "totem"),
         ]
 
-    def realizar_jogada(self, x: int, y: int, color: str, symbol: str) -> dict:
+    def realizar_jogada(self, x, y, color) -> dict:
+        if self.match_state != MatchStateEnum.WAITING_INPUT:
+            messagebox.showerror("Jogada inválida", "Não é o seu turno!")
+            return
+
+        match self.fase_jogada:
+            case JogadaFase.SELECIONAR_TOTEM:
+                peca = self.get_peca(x, y)
+                if peca and peca.type == "totem":
+                    self.totem_selecionado = peca
+                    self.fase_jogada = JogadaFase.MOVER_TOTEM
+                    messagebox.showinfo("Totem selecionado", f"Totem em ({x}, {y}) selecionado.")
+                else:
+                    messagebox.showwarning("Seleção inválida", "Selecione um Totem válido (preto).")
+
+            case JogadaFase.MOVER_TOTEM:
+                if self.get_peca(x, y):
+                    messagebox.showwarning("Movimento inválido", "Já existe uma peça nessa posição.")
+                    return
+                
+                # Verifica movimento ortogonal do totem
+                from_x = self.totem_selecionado.x
+                from_y = self.totem_selecionado.y
+
+                movimento_diagonal = from_x != x and from_y != y
+
+                if movimento_diagonal:
+                    ortogonal_disponivel = False
+
+                    # Verifica todas as casas na mesma linha (x fixo) e coluna (y fixo)
+                    for i in range(5):
+                        if i != from_y and not self.get_peca(from_x, i):
+                            ortogonal_disponivel = True
+                            break
+                        if i != from_x and not self.get_peca(i, from_y):
+                            ortogonal_disponivel = True
+                            break
+
+                    if ortogonal_disponivel:
+                        messagebox.showwarning("Movimento inválido", "Movimento diagonal não permitido enquanto houver casas ortogonais livres.")
+                        return
+                            
+                self.totem_selecionado.x = x
+                self.totem_selecionado.y = y
+                self.composicao_jogada = {
+                    "totem": {
+                    "x": x,
+                    "y": y,
+                    "symbol": self.totem_selecionado.symbol
+                    }
+                }
+                self.totem_selecionado = None
+
+                self.fase_jogada = JogadaFase.ESCOLHER_SIMBOLO
+                messagebox.showinfo("Totem movido", "Agora selecione um símbolo (X ou O).")
+
+            case JogadaFase.ESCOLHER_SIMBOLO:
+                if self.simbolo_escolhido == None:
+                    messagebox.showwarning("Movimento inválido", "Você deve selecionar um simbolo")
+                    return
+                
+            case JogadaFase.COLOCAR_PECA:
+                if self.simbolo_escolhido is None:
+                    messagebox.showwarning("Símbolo não escolhido", "Você precisa escolher X ou O antes.")
+                    return
+                if self.get_peca(x, y):
+                    messagebox.showwarning("Posição ocupada", "Essa posição já está ocupada.")
+                    return
+
+                totem_x = self.composicao_jogada["totem"]["x"]
+                totem_y = self.composicao_jogada["totem"]["y"]
+
+                adjacentes_vazias = self.get_posicoes_adjacentes_vazias(totem_x, totem_y)
+
+                if adjacentes_vazias:
+                    if (x, y) not in adjacentes_vazias:
+                        messagebox.showwarning(
+                            "Movimento inválido",
+                            "Você só pode colocar a peça em uma célula adjacente vazia ao Totem movido."
+                        )
+                        return
+
+                nova_peca = self.adicionar_nova_peca(x, y, color, self.simbolo_escolhido)
+
+                condicao_vitoria_atingida = self.verificar_vitoria(x, y, color, self.simbolo_escolhido)
+
+                match_status = "next"
+                if condicao_vitoria_atingida:
+                    match_status = "finished"
+                    messagebox.showwarning("Partida finalizada", "você venceu!")
+                    self.match_state = MatchStateEnum.FINISHED_BY_VICTORY_CONDITION
+                else:
+                    self.match_state = MatchStateEnum.WAITING_REMOTE
+                    self.fase_jogada = JogadaFase.SELECIONAR_TOTEM
+                    self.simbolo_escolhido = None
+
+                self.composicao_jogada["peca"] = nova_peca
+                self.composicao_jogada["match_status"] = match_status
+                
+                return self.composicao_jogada
+
+    def adicionar_nova_peca(self, x: int, y: int, color: str, symbol: str) -> dict:
         if 0 <= x < self.grid_size and 0 <= y < self.grid_size:
             if not any(p.x == x and p.y == y for p in self.pieces):
                 peca = Peca(x, y, color, symbol, "pedra")
@@ -82,3 +202,49 @@ class Tabuleiro:
             if peca.x == x and peca.y == y:
                 return peca
         return None
+    
+    def get_posicoes_adjacentes_vazias(self, x: int, y: int):
+        adjacentes = []
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                nx, ny = x + dx, y + dy
+                if dx == 0 and dy == 0:
+                    continue
+                if 0 <= nx < self.grid_size and 0 <= ny < self.grid_size:
+                    if not self.get_peca(nx, ny):
+                        adjacentes.append((nx, ny))
+        return adjacentes
+
+    def verificar_vitoria(self, x, y, cor, simbolo):
+        direcoes = [
+            (1, 0),  
+            (0, 1),  
+            (1, 1),  
+            (1, -1), 
+        ]
+
+        for dx, dy in direcoes:
+            cont_cor = 1
+            cont_simbolo = 1
+
+            for sentido in [-1, 1]:
+                nx, ny = x, y
+                while True:
+                    nx += dx * sentido
+                    ny += dy * sentido
+                    if 0 <= nx < 5 and 0 <= ny < 5:
+                        peca = self.get_peca(nx, ny)
+                        if not peca or peca.type == "totem":
+                            break
+
+                        if peca.color == cor:
+                            cont_cor += 1
+                        if peca.symbol == simbolo:
+                            cont_simbolo += 1
+                    else:
+                        break
+
+            if cont_cor >= 4 or cont_simbolo >= 4:
+                return True
+
+        return False
